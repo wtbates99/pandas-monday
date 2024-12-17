66b6859a570654568c5628f8ea1038e39090b53e
wtbates99 <wtbates99@gmail.com>
2024-12-17 16:04:35 -0500

    clean up exceptions

--- pandas_monday/exceptions.py
+++ pandas_monday/exceptions.py
@@ -10,43 +10,13 @@ class MondayAPIError(MondayError):
     pass
 
 
-class MondayValidationError(MondayError):
-    """Raised when there is a validation error in the input parameters."""
-
-    pass
-
-
 class InvalidColumnOrder(MondayError):
     """Raised when the provided column order does not match the available columns."""
 
     pass
 
 
-class InvalidSchema(MondayError):
-    """Raised when there is a mismatch between DataFrame and Monday.com column types."""
-
-    pass
-
-
 class BoardNotFoundError(MondayError):
     """Raised when the specified Monday.com board cannot be found."""
-
-    pass
-
-
-class WorkspaceNotFoundError(MondayError):
-    """Raised when the specified Monday.com workspace cannot be found."""
-
-    pass
-
-
-class RateLimitExceededError(MondayAPIError):
-    """Raised when the Monday.com API rate limit is exceeded."""
-
-    pass
-
-
-class AuthenticationError(MondayAPIError):
-    """Raised when there is an authentication error with the Monday.com API."""
 
     pass
